46e65f69f002794ffe2ffd9ce9ea313eb47b5a26
Re-zan <zannat881all@gmail.com>
2023-09-10 16:57:44 +0600

    voluanteer and events route creation

--- index.js
+++ index.js
@@ -35,10 +35,50 @@ async function run() {
     const vlounters = dbName.collection("vlountertem-collentions");
     const blogs = dbName.collection("blogsDatas");
 
-    ////////////////////////////////events part
+    //////////////////////////////////////////events part
     //get data
     app.get("/events", async (req, res) => {
-      const result = await events.find().toArray();
+      const result = await events.find().limit(18).sort({ _id: -1 }).toArray();
+      res.send(result);
+    });
+
+    //create events
+    app.post("/events", async (req, res) => {
+      const data = req.body;
+      const result = await events.insertOne(data);
+      res.send(result);
+    });
+
+    //status upadete
+    app.patch("/events/status/upcoming/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updetData = {
+        $set: {
+          status: "upcoming",
+        },
+      };
+      const result = await events.updateOne(filter, updetData);
+      res.send(result);
+    });
+
+    app.patch("/events/status/cancel/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updetData = {
+        $set: {
+          status: "canceled",
+        },
+      };
+      const result = await events.updateOne(filter, updetData);
+      res.send(result);
+    });
+
+    //delete items
+    app.delete("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await events.deleteOne(filter);
       res.send(result);
     });
 
@@ -47,13 +87,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await events.findOne(query);
-      res.send(result);
-    });
-
-    //create events
-    app.post("/events", async (req, res) => {
-      const data = req.body;
-      const result = await events.insertOne(data);
       res.send(result);
     });
 
@@ -75,7 +108,11 @@ async function run() {
     ////////////////////////////////vlounters part
     //get data
     app.get("/vlounters", async (req, res) => {
-      const result = await vlounters.find().toArray();
+      const result = await vlounters
+        .find()
+        .limit(18)
+        .sort({ _id: -1 })
+        .toArray();
       res.send(result);
     });
 
@@ -84,6 +121,19 @@ async function run() {
       const data = req.body;
       const resutl = await vlounters.insertOne(data);
       res.send(resutl);
+    });
+
+    //make voluanteer
+    app.put("/vlounters/voluanteer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const upData = {
+        $set: {
+          role: "voluanteer",
+        },
+      };
+      const result = await vlounters.updateOne(query, upData);
+      res.send(result);
     });
 
     /////////////////////////////////their story part
@@ -107,7 +157,7 @@ run().catch(console.dir);
 
 //testing
 app.get("/", (req, res) => {
-  res.send("Data are cooking.....................");
+  res.send("Datas are cooking.....................");
 });
 
 //make connection
