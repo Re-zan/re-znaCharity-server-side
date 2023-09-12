93af625546fe47208ade9d81fe437373e542139c
Re-zan <zannat881all@gmail.com>
2023-09-12 23:18:41 +0600

    dashboard done for admin and donar

--- index.js
+++ index.js
@@ -34,6 +34,61 @@ async function run() {
     const events = dbName.collection("eventsCollections");
     const vlounters = dbName.collection("vlountertem-collentions");
     const blogs = dbName.collection("blogsDatas");
+    const users = dbName.collection("user-collection");
+
+    //////////////////////////////////////////////user part
+    //get user
+    app.get("/users", async (req, res) => {
+      const result = await users.find().limit(18).sort({ _id: -1 }).toArray();
+      res.send(result);
+    });
+
+    //create user
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      const query = { email: data.email };
+      const exiting = await users.findOne(query);
+      if (exiting) {
+        return res.send("You are already in my db");
+      }
+      const result = await users.insertOne(data);
+      res.send(result);
+    });
+
+    //make admin
+    app.put("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const upData = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await users.updateOne(query, upData);
+      res.send(result);
+    });
+
+    // get admi data
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await users.findOne(filter);
+
+      res.send(result);
+    });
+
+    //make donar
+    app.put("/users/donar/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const upData = {
+        $set: {
+          role: "donar",
+        },
+      };
+      const result = await users.updateOne(query, upData);
+      res.send(result);
+    });
 
     //////////////////////////////////////////events part
     //get data
@@ -42,10 +97,50 @@ async function run() {
       res.send(result);
     });
 
+    //get single data
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await events.findOne(query);
+      res.send(result);
+    });
+
     //create events
     app.post("/events", async (req, res) => {
       const data = req.body;
       const result = await events.insertOne(data);
+      res.send(result);
+    });
+
+    //update data
+    app.patch("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updetData = {
+        $set: {
+          title: data?.title,
+          short_title: data.short_title,
+          description: data.description,
+          location: data.location,
+          date: data.date,
+          image: data?.image,
+        },
+      };
+      // if (data?.image) {
+      //   updateData.$set.image = data.image;
+      //   updateData.$set.short_title = data.short_title;
+      //   updateData.$set.description = data.description;
+      //   updateData.$set.location = data.location;
+      //   updateData.$set.date = data.date;
+      // } else {
+      //   updateData.$set.short_title = data.short_title;
+      //   updateData.$set.description = data.description;
+      //   updateData.$set.location = data.location;
+      //   updateData.$set.date = data.date;
+      // }
+
+      const result = await events.updateOne(filter, updetData);
       res.send(result);
     });
 
@@ -79,14 +174,6 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await events.deleteOne(filter);
-      res.send(result);
-    });
-
-    //get single data
-    app.get("/events/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await events.findOne(query);
       res.send(result);
     });
 
@@ -143,6 +230,7 @@ async function run() {
       res.send(result);
     });
 
+    //////project end
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
